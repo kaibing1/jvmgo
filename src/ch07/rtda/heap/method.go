c843296a3d2d9a7b812d0003527356a1a0b20802
kaibing1 <yang1148436163@163.com>
2021-06-27 19:21:44 +0900

    update the ch07

--- src/ch07/rtda/heap/method.go
+++ src/ch07/rtda/heap/method.go
@@ -4,9 +4,10 @@ import "jvmgo/ch07/classfile"
 
 type Method struct {
 	ClassMember
-	maxStack  uint
-	maxLocals uint
-	code      []byte
+	maxStack     uint
+	maxLocals    uint
+	code         []byte
+	argSlotCount uint
 }
 
 func newMethods(class *Class, cfMethods []*classfile.MemberInfo) []*Method {
@@ -16,6 +17,7 @@ func newMethods(class *Class, cfMethods []*classfile.MemberInfo) []*Method {
 		methods[i].class = class
 		methods[i].copyMemberInfo(cfMethod)
 		methods[i].copyAttributes(cfMethod)
+		methods[i].calArgSlotCount()
 	}
 	return methods
 }
@@ -27,6 +29,37 @@ func (self *Method) copyAttributes(cfMethod *classfile.MemberInfo) {
 		self.code = codeAttr.Code()
 	}
 }
+func (self *Method) calArgSlotCount() {
+	parsedDescriptor := parseMethodDescriptor(self.descriptor)
+	for _, paramType := range parsedDescriptor.parameterTypes {
+		self.argSlotCount++
+		if paramType == "J" || paramType == "D" {
+			self.argSlotCount++
+		}
+	}
+	if !self.IsStatic() {
+		self.argSlotCount++
+	}
+}
+
+func (self *Method) IsSynchronized() bool {
+	return 0 != self.accessFlags&ACC_SYNCHRONIZED
+}
+func (self *Method) IsBridge() bool {
+	return 0 != self.accessFlags&ACC_BRIDGE
+}
+func (self *Method) IsVarargs() bool {
+	return 0 != self.accessFlags&ACC_VARARGS
+}
+func (self *Method) IsNative() bool {
+	return 0 != self.accessFlags&ACC_NATIVE
+}
+func (self *Method) IsAbstract() bool {
+	return 0 != self.accessFlags&ACC_ABSTRACT
+}
+func (self *Method) IsStrict() bool {
+	return 0 != self.accessFlags&ACC_STRICT
+}
 
 func (self *Method) MaxLocals() uint {
 	return self.maxLocals
@@ -37,4 +70,8 @@ func (self *Method) MaxStack() uint {
 
 func (self *Method) Code() []byte {
 	return self.code
+}
+
+func (self *Method) ArgSlotCount() uint {
+	return self.argSlotCount
 }
