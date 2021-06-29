f5b065b6d3b7f6a09ec7316948bbd00383fcfe97
kaibing1 <yang1148436163@163.com>
2021-06-29 16:24:21 +0900

    update the ch08

---
+++ src/ch08/instructions/control/return.go
@@ -0,0 +1,74 @@
+package control
+
+import (
+	"jvmgo/ch08/instructions/base"
+	"jvmgo/ch08/rtda"
+)
+
+type RETURN struct {
+	base.NoOperandsInstruction
+}
+
+func (self *RETURN) Execute(frame *rtda.Frame) {
+	frame.Thread().PopFrame()
+}
+
+type ARETURN struct {
+	base.NoOperandsInstruction
+}
+
+func (self *ARETURN) Execute(frame *rtda.Frame) {
+	thread := frame.Thread()
+	currentFrame := thread.PopFrame()
+	invokerFrame := thread.TopFrame()
+	ref := currentFrame.OperandStack().PopRef()
+	invokerFrame.OperandStack().PushRef(ref)
+}
+
+type DRETURN struct {
+	base.NoOperandsInstruction
+}
+
+func (self *DRETURN) Execute(frame *rtda.Frame) {
+	thread := frame.Thread()
+	currentFrame := thread.PopFrame()
+	invokerFrame := thread.TopFrame()
+	val := currentFrame.OperandStack().PopDouble()
+	invokerFrame.OperandStack().PushDouble(val)
+}
+
+type FRETURN struct {
+	base.NoOperandsInstruction
+}
+
+func (self *FRETURN) Execute(frame *rtda.Frame) {
+	thread := frame.Thread()
+	currentFrame := thread.PopFrame()
+	invokerFrame := thread.TopFrame()
+	val := currentFrame.OperandStack().PopFloat()
+	invokerFrame.OperandStack().PushFloat(val)
+}
+
+type IRETURN struct {
+	base.NoOperandsInstruction
+}
+
+func (self *IRETURN) Execute(frame *rtda.Frame) {
+	thread := frame.Thread()
+	currentFrame := thread.PopFrame()
+	invokerFrame := thread.TopFrame()
+	val := currentFrame.OperandStack().PopInt()
+	invokerFrame.OperandStack().PushInt(val)
+}
+
+type LRETURN struct {
+	base.NoOperandsInstruction
+}
+
+func (self *LRETURN) Execute(frame *rtda.Frame) {
+	thread := frame.Thread()
+	currentFrame := thread.PopFrame()
+	invokerFrame := thread.TopFrame()
+	val := currentFrame.OperandStack().PopLong()
+	invokerFrame.OperandStack().PushLong(val)
+}
---
+++ src/ch08/rtda/heap/object.go
@@ -0,0 +1,24 @@
+package heap
+
+type Object struct {
+	class *Class
+	data  interface{}
+}
+
+func newObject(class *Class) *Object {
+	return &Object{
+		class: class,
+		data:  newSlots(class.instanceSlotCount),
+	}
+}
+
+func (self *Object) Class() *Class {
+	return self.class
+}
+func (self *Object) Fields() Slots {
+	return self.data.(Slots)
+}
+
+func (self *Object) IsInstanceOf(class *Class) bool {
+	return class.IsAssignableFrom(self.class)
+}
